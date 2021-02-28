7d50bd640d1295fe5c103a51bd205d6fd2ff5929
Lucky Prabath <lucky.prabath94@gmail.com>
2021-02-28 19:22:28 +0530

    Create all DTOs

---
+++ src/main/java/lk/ijse/dep/web/dto/ErrorDTO.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author : Lucky Prabath <lucky.prabath94@gmail.com>
+ * @since : 2021-02-28
+ **/
+
+@Data @NoArgsConstructor @AllArgsConstructor
+public class ErrorDTO implements Serializable {
+    private int status;
+    private String error;
+    private String message;
+}
