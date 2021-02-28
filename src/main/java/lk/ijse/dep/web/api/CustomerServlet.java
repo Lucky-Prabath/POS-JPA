9b2a86e41026d6f235f2115ba705ba7fed7d7467
Lucky Prabath <lucky.prabath94@gmail.com>
2021-02-28 17:14:12 +0530

    Create package structure

---
+++ src/main/java/lk/ijse/dep/web/api/CustomerServlet.java
@@ -0,0 +1,23 @@
+package lk.ijse.dep.web.api;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author : Lucky Prabath <lucky.prabath94@gmail.com>
+ * @since : 2021-02-28
+ **/
+@WebServlet(name = "CustomerServlet")
+public class CustomerServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
