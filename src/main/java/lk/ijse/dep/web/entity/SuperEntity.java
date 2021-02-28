f32791c8681a4048f6339fd0e87a47f468082b03
Lucky Prabath <lucky.prabath94@gmail.com>
2021-02-28 19:08:02 +0530

    Create all entities

---
+++ src/main/java/lk/ijse/dep/web/entity/SuperEntity.java
@@ -0,0 +1,10 @@
+package lk.ijse.dep.web.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author : Lucky Prabath <lucky.prabath94@gmail.com>
+ * @since : 2021-02-28
+ **/
+public interface SuperEntity extends Serializable {
+}
