0110780b5155de888c224fec1932e55c7998444c
Lucky Prabath <lucky.prabath94@gmail.com>
2021-02-28 22:33:58 +0530

    Create AppConfig and AppInitializer classes

---
+++ src/main/java/lk/ijse/dep/web/AppConfig.java
@@ -0,0 +1,15 @@
+package lk.ijse.dep.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : Lucky Prabath <lucky.prabath94@gmail.com>
+ * @since : 2021-02-28
+ **/
+
+@Configuration
+@ComponentScan
+//todo: add aspectjautoproxy to hangle transactions in bo layer
+public class AppConfig {
+}
